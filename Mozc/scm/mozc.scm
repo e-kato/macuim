1eecba497d04b5bf6c998cfeea70fd986e597d38
ek.kato <e-kato@users.noreply.github.com>
2012-01-12 02:21:13 +0000

    * fix for issue #11

--- Mozc/scm/mozc.scm
+++ Mozc/scm/mozc.scm
@@ -77,7 +77,6 @@
                         #f))
                      ((= 0 pid2)
                       (setenv "MALLOC_CHECK_" "0" 0)
-                      (setenv "GTK_IM_MODULE" "gtk-im-context-simple" 0)
                       (if (= (process-execute file argv) -1)
                         (uim-notify-fatal (format (N_ "cannot execute ~a") file)))
                       (_exit 0))
